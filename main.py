d06be14f9f899d75bbe641443e63bf0d31415ffb
KAdenrele <akomolafe@zaizi.com>
2026-01-21 19:21:44 +0000

    directories and dockerfile updated

--- main.py
+++ main.py
@@ -7,30 +7,29 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 BASE_DIR = "/data"
-DEST_BASE = os.path.join(BASE_DIR, "data2/sample_curated/images")
+DEST_BASE = os.path.join(BASE_DIR, "data/curated/images")
 
 if __name__ == "__main__":
-      #mavos_dataset = load_dataset("unibuc-cs/MAVOS-DD", cache_dir=MAVOS_DIR, num_proc=os.cpu_count())
     run_pipeline(
         dataset_name="SAFE",
-        image_directory_path=os.path.join(BASE_DIR, "data2/training_data/SAFE/data"),
+        image_directory_path=os.path.join(BASE_DIR, "training_data/SAFE/data"),
         destination_directory=os.path.join(DEST_BASE, "SAFE"),
         is_huggingface=False,
         has_subdirectories=True,
         is_synthetic=True,
         simulations_to_run=ALL_SIMULATIONS,
-        target_sample_size=10
+        target_sample_size=2000
     )
     run_pipeline(
         dataset_name="COCO",
-        image_directory_path=os.path.join(BASE_DIR, "data/raw/coco_images_authentic"),
+        image_directory_path=os.path.join(BASE_DIR, "test_datasetcd/raw/coco_images_authentic"),
         destination_directory=os.path.join(DEST_BASE, "COCO"),
         is_huggingface=True,
         has_subdirectories=False,
         is_synthetic=False,
         simulations_to_run=ALL_SIMULATIONS,
         hf_name="detection-datasets/coco",
-        target_sample_size=10
+        target_sample_size=2000
     )
 
     logging.info("--- Main process complete. All datasets downloaded and curated. ---")
--- run_docker_process.sh
+++ run_docker_process.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status.
 set -e
-docker build -t test-data-app . && docker run --rm --gpus all -v /mnt/data2/test_dataset/curated:/data
+docker build -t test-data-app . && docker run --rm --gpus all -v /mnt/data2/:/data
