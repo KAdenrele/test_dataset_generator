9761245c9e156f601db32607254e3b3225675e1a
KAdenrele <akomolafe@zaizi.com>
2026-01-21 19:39:30 +0000

    corrected destination directory

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 BASE_DIR = "/data"
-DEST_BASE = os.path.join(BASE_DIR, "data/curated/images")
+DEST_BASE = os.path.join(BASE_DIR, "test_dataset/data/curated/images")
 
 if __name__ == "__main__":
     run_pipeline(
