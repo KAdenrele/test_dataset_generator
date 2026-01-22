f61413ce640423342b908bc3c314cb732b94bae1
KAdenrele <akomolafe@zaizi.com>
2026-01-22 00:00:20 +0000

    fixed destination directory

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 BASE_DIR = "/data"
-DEST_BASE = os.path.join(BASE_DIR, "test_dataset/data/curated/images")
+DEST_BASE = os.path.join(BASE_DIR, "test_dataset/curated/images")
 
 if __name__ == "__main__":
     run_pipeline(
@@ -29,6 +29,16 @@ if __name__ == "__main__":
         is_synthetic=False,
         simulations_to_run=ALL_SIMULATIONS,
         hf_name="detection-datasets/coco",
+        target_sample_size=2000
+    )
+    run_pipeline(
+        dataset_name="Inswapper",
+        image_directory_path=os.path.join(BASE_DIR, "test_dataset/raw/inswapper_images"),
+        destination_directory=os.path.join(DEST_BASE, "Inswapper"),
+        is_huggingface=False,
+        has_subdirectories=False,
+        is_synthetic=True,
+        simulations_to_run=ALL_SIMULATIONS,
         target_sample_size=2000
     )
 
